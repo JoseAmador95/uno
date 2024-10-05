607a1defc7c8cf9739ef5c35cc5ebd21feb5eb3a
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-10-05 15:53:42 +0100

    Refactor card presentation in stdout

--- src/card.rs
+++ src/card.rs
@@ -28,20 +28,32 @@ pub struct Card {
 impl std::fmt::Display for Card {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let colour = match self.colour {
-            Colour::Blue => "blue".blue(),
-            Colour::Red => "red".red(),
-            Colour::Yellow => "yellow".yellow(),
-            Colour::Green => "green".green(),
-            _ => "".bold(),
+            Colour::Blue => "Blue".blue(),
+            Colour::Red => "Red".red(),
+            Colour::Yellow => "Yellow".yellow(),
+            Colour::Green => "Green".green(),
+            Colour::Wild => format!(
+                "{w}{i}{l}{d}",
+                w = "W".red(),
+                i = "i".yellow(),
+                l = "l".green(),
+                d = "d".blue()
+            )
+            .normal(),
         };
         let value = match self.value {
             CardValue::DrawTwo => "Draw Two".to_string(),
             CardValue::Reverse => "Reverse".to_string(),
             CardValue::Skip => "Skip".to_string(),
-            CardValue::Number(n) => format!("Number {n}"),
-            CardValue::Wild => "Wild".to_string(),
-            CardValue::WildDraw(n) => format!("Wild Draw +{n}"),
+            CardValue::Number(n) => n.to_string(),
+            CardValue::Wild => "Select Color".to_string(),
+            CardValue::WildDraw(n) => format!("Draw +{n}"),
         };
-        write!(f, "{colour} {value}")
+
+        if colour.is_empty() {
+            write!(f, "{value}")
+        } else {
+            write!(f, "{colour} {value}")
+        }
     }
 }
