7618a12878809539a01e4fed255c2680a6f506f5
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-10-06 13:50:23 +0100

    Switched the deck logic from Vec to VecDeque

--- src/deck.rs
+++ src/deck.rs
@@ -1,28 +1,31 @@
 use crate::card::Card;
 use crate::default_deck::GAME_DECK;
 use rand::{seq::SliceRandom, thread_rng};
+use std::collections::VecDeque;
 
 pub struct Deck {
-    draw_pile: Vec<Card>,
-    discard_pile: Vec<Card>,
+    draw_pile: VecDeque<Card>,
+    discard_pile: VecDeque<Card>,
 }
 
 impl Deck {
     pub fn draw(&mut self) -> Option<Card> {
-        self.draw_pile.pop()
+        self.draw_pile.pop_front()
     }
 
     pub fn discard(&mut self, card: Card) {
-        self.discard_pile.push(card);
+        self.discard_pile.push_back(card);
     }
 
     pub fn shuffle(&mut self) {
         let mut rng = thread_rng();
-        self.draw_pile.shuffle(&mut rng);
+        let mut card_vec: Vec<Card> = self.draw_pile.drain(..).collect();
+        card_vec.shuffle(&mut rng);
+        self.draw_pile = VecDeque::from(card_vec);
     }
 
     pub fn get_top_card(&self) -> Option<&Card> {
-        self.discard_pile.last()
+        self.discard_pile.back()
     }
 
     pub fn draw_pile_is_empty(&self) -> bool {
@@ -30,14 +33,24 @@ impl Deck {
     }
 
     pub fn refill_draw_pile(&mut self) {
+        assert!(
+            !self.draw_pile_is_empty() || !self.discard_pile.is_empty(),
+            "Both discard and draw piles are empty"
+        );
+        let top_card = self.discard_pile.pop_back();
         self.draw_pile.append(&mut self.discard_pile);
         self.discard_pile.clear();
         self.shuffle();
-        self.discard_from_draw_pile();
+
+        if let Some(c) = top_card {
+            self.discard(c);
+        } else if !self.draw_pile_is_empty() {
+            self.discard_from_draw_pile();
+        }
     }
 
     pub fn discard_from_draw_pile(&mut self) {
-        if let Some(card) = self.draw_pile.pop() {
+        if let Some(card) = self.draw() {
             self.discard(card);
         }
     }
@@ -48,8 +61,8 @@ impl Deck {
 
     pub fn new() -> Self {
         let mut deck = Deck {
-            draw_pile: GAME_DECK.to_vec(),
-            discard_pile: Vec::new(),
+            draw_pile: VecDeque::from(GAME_DECK.to_vec()),
+            discard_pile: VecDeque::new(),
         };
         deck.shuffle();
         deck.discard_from_draw_pile();
