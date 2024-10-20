7f77aebca9522b2aa5753708edf14f74cf35f860
Jose Amador Demeneghi <20510893+JoseAmador95@users.noreply.github.com>
2024-10-20 19:53:16 +0100

    Document deck.rs

--- src/deck.rs
+++ src/deck.rs
@@ -4,27 +4,47 @@ use mockall::automock;
 use rand::{seq::SliceRandom, thread_rng};
 use std::collections::VecDeque;
 
+/// Type alias for results returned by deck operations.
 type DeckResult<T> = Result<T, Error>;
 
+/// Enum representing possible errors in deck operations.
 #[derive(Debug, PartialEq)]
 pub enum Error {
     DrawPileIsEmpty,
     DiscardPileIsEmpty,
 }
 
+/// Struct representing a deck of cards with draw and discard piles.
 pub struct Deck {
     draw_pile: VecDeque<card::Card>,
     discard_pile: VecDeque<card::Card>,
 }
 
 #[automock]
+/// Trait defining the operations that can be performed on a deck.
 pub trait DeckTrait {
+    /// Draws a card from the draw pile. The card is removed from the draw pile.
+    /// The function returns the drawn card if successful, otherwise an error.
     fn draw(&mut self) -> DeckResult<card::Card>;
+
+    /// Discards a card to the discard pile.
     fn discard(&mut self, card: card::Card);
+
+    /// Gets the top card from the discard pile.
+    /// The function returns a reference to the top card if successful, otherwise an error.
     fn get_top_card<'a>(&'a self) -> DeckResult<&'a card::Card>;
+
+    /// Refills the draw pile from the discard pile.
+    /// The function returns `Ok(())` if successful, otherwise an error.
     fn refill_draw_pile(&mut self) -> DeckResult<()>;
+
+    /// Returns the number of cards in the draw pile.
     fn number_of_cards_in_draw_pile(&self) -> usize;
+
+    /// Changes the color of the top card in the discard pile.
     fn change_colour_of_top_card_in_discard(&mut self, colour: &card::Colour);
+
+    /// Creates a new deck, optionally using a provided set of cards.
     fn new(deck: Option<Vec<card::Card>>) -> Self;
 }
 
@@ -33,32 +53,32 @@ impl DeckTrait for Deck {
         if let Some(c) = self.draw_pile.pop_front() {
             return Ok(c);
         }
-
         Err(Error::DrawPileIsEmpty)
     }
 
     fn discard(&mut self, card: card::Card) {
         self.discard_pile.push_back(card);
     }
+
     fn refill_draw_pile(&mut self) -> DeckResult<()> {
         if let Some(card) = self.discard_pile.pop_back() {
-            self.rever_wild_cards_in_discard_pile();
+            self.revert_wild_cards_in_discard_pile();
             self.draw_pile.append(&mut self.discard_pile);
             self.discard_pile.clear();
             self.shuffle();
             self.discard(card);
             return Ok(());
         }
-
         Err(Error::DiscardPileIsEmpty)
     }
+
     fn get_top_card<'a>(&'a self) -> DeckResult<&'a card::Card> {
         if let Some(c) = self.discard_pile.back() {
             return Ok(c);
         }
-
         Err(Error::DiscardPileIsEmpty)
     }
+
     fn number_of_cards_in_draw_pile(&self) -> usize {
         self.draw_pile.len()
     }
@@ -75,7 +95,6 @@ impl DeckTrait for Deck {
         } else {
             GAME_DECK.to_vec()
         };
-
         let mut deck = Deck {
             draw_pile: VecDeque::from(card_set),
             discard_pile: VecDeque::new(),
@@ -90,6 +109,7 @@ impl DeckTrait for Deck {
 }
 
 impl Deck {
+    /// Shuffles the draw pile randomly.
     fn shuffle(&mut self) {
         let mut rng = thread_rng();
         let mut card_vec: Vec<card::Card> = self.draw_pile.drain(..).collect();
@@ -97,19 +117,23 @@ impl Deck {
         self.draw_pile = VecDeque::from(card_vec);
     }
 
-    fn rever_wild_cards_in_discard_pile(&mut self) {
+    /// Reverts wild cards in the discard pile to their original color.
+    fn revert_wild_cards_in_discard_pile(&mut self) {
         let _ = self
             .discard_pile
             .iter_mut()
             .filter(|card| matches!(card.value, card::Value::Wild | card::Value::WildDraw(_)))
-            .map(|card| card.colour = card::Colour::Wild)
             .collect::<Vec<_>>();
     }
 
+    /// Discards a card from the draw pile to the discard pile.
     fn discard_from_draw_pile(&mut self) -> DeckResult<()> {
-        let card = self.draw()?;
-        self.discard(card);
-        Ok(())
+        if let Some(card) = self.draw_pile.pop_front() {
+            self.discard(card);
+            Ok(())
+        } else {
+            Err(Error::DrawPileIsEmpty)
+        }
     }
 }
 
@@ -133,7 +157,6 @@ mod tests {
         };
         let mut deck = create_test_deck();
         deck.draw_pile.push_back(card);
-
         let result = deck.draw();
         assert_eq!(result, DeckResult::Ok(card));
     }
@@ -294,7 +317,7 @@ mod tests {
             value: Value::Wild,
         };
         deck.discard(card);
-        deck.rever_wild_cards_in_discard_pile();
+        deck.revert_wild_cards_in_discard_pile();
         assert_eq!(deck.discard_pile.back().unwrap().colour, Colour::Wild);
     }
 
